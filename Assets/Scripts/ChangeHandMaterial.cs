15953fd582e53ed0a2599740bae7f40d9d10b441
NadaMoAlyami <nada.alyami.ksa@outlook.com>
2025-04-21 18:46:46 +0300

    Hand Menu

---
+++ Assets/Scripts/ChangeHandMaterial.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ChangeHandMaterial : MonoBehaviour
+{
+    public SkinnedMeshRenderer leftHandRenderer;
+    public SkinnedMeshRenderer rightHandRenderer;
+    public Material newMaterial;
+
+    public void ChangeMaterial()
+    {
+        leftHandRenderer.material = newMaterial;
+        rightHandRenderer.material = newMaterial;
+    }
+}
